cd07065306b386f286d5b6efa691e593e24fefdc
Sahil Kasmani <sahilkasmani7176@gmail.com>
2023-07-12 20:58:59 +0530

    add all files for commit

---
+++ src/Create.js
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
+const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const [pending,setPending] = useState(true);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      const blog = {title,body,author};
+
+      setPending(false);
+      setTimeout(() => {
+        fetch('http://localhost:8000/blogs',{
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(blog)
+        })
+        .then(() =>{
+          console.log("new blog added");
+          setPending(true);
+        })
+      }, 100);
+      navigate('/');
+    }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          value={title}
+          required
+          onChange={(e) => { setTitle(e.target.value) }}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => { setBody(e.target.value) }}
+        ></textarea>
+        <label>Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => { setAuthor(e.target.value) }}
+        >
+          <option value="danish">danish</option>
+          <option value="john">john</option>
+        </select>
+        { pending && <button>Add Blog</button>}
+        { !pending && <button disabled>Adding Blog...</button>}
+      </form>
+    </div>
+  )
+}
+
+export default Create
